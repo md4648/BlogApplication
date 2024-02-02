e12c2808e852dca4b81a14222f985691c0616252
md4648 <124080342+md4648@users.noreply.github.com>
2024-02-03 01:23:23 +0300

    make dashboard Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import  Header from "./componets/Header"
 import Footer from './componets/Footer';
+import PrivateRoute from './componets/PrivateRoute';
 export default function App() {
   return (
   
@@ -17,7 +18,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,10 @@ import userReducer from './user/userSlice';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import themeReducer from './theme/themeSlice';
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
