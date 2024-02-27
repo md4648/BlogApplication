c6d18cd0d585425278149edb91c95466bdd049c7
md4648 <124080342+md4648@users.noreply.github.com>
2024-02-27 14:19:56 +0300

    show commnets of post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from './pages/PostPage';
 
-import ScrollToTop from './components/ScrollToTop';// cheak why we use
+import ScrollToTop from './componets/ScrollToTop';// cheak why we use
 
 export default function App() {
   return (
